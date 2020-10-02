0f182dc62c56b41a52c7dd8731f51072b72c5abb
h181214 <carstends@gmail.com>
2020-10-02 19:41:06 +0200

    First commit

---
+++ src/de/vogella/jpa/simple/main/App.java
@@ -0,0 +1,64 @@
+package de.vogella.jpa.simple.main;
+
+import static spark.Spark.*;
+
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import de.vogella.jpa.simple.DAO.TodoDAO;
+import de.vogella.jpa.simple.model.Todo;
+
+public class App {
+
+	static Gson gson = new Gson();
+    static TodoDAO todoDAO = new TodoDAO();
+
+    
+    
+    
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            port(Integer.parseInt(args[0]));
+        } else {
+            port(8080);
+        }
+
+        after((req, res) -> {
+            res.type("application/json");
+        });
+
+        get("/todos", (req, res) -> {
+            List<Todo> todos = todoDAO.read();
+            Gson gson = new Gson();
+            return gson.toJson(todos.toArray());
+        });
+
+        get("/todo/:id", (req, res) -> {
+            Todo todo = todoDAO.read(Long.parseLong(req.params(":id")));
+            return gson.toJson(todo);
+            
+        });
+
+        put("/todo", (req, res) -> {	
+        	Todo todo = gson.fromJson(req.body(), Todo.class); 
+            
+            todoDAO.update(todo);
+            return "Success";
+        });
+
+        post("/todo", (req, res) -> {
+        	
+            Todo todo = gson.fromJson(req.body(), Todo.class); 
+            todoDAO.create(todo);
+            return gson.toJson(todo);
+        });
+
+        delete("/todo/:id", (req, res) -> {
+            todoDAO.delete(Long.parseLong(req.params(":id")));
+            return "Success";
+        });
+    }
+
+}
